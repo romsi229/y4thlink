caf046c0b815060bce16b40ba80a8405754c0802
GBOTA ROMULUS <gbotaromulus@MacBook-Neo-de-GBOTA.local>
2026-06-02 03:44:55 +0100

    Interface Y4thLink ajoutée

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from app.routes.chat import router
 
 app = FastAPI(title="Y4thLink API")
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.include_router(router)
 
 @app.get("/")
 def home():
-    return JSONResponse(
-        content={"message": "Y4thLink est en ligne 🌿"},
-        media_type="application/json; charset=utf-8"
-    )
+    return FileResponse("app/static/index.html")
